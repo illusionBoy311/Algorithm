6f56a984ab3f51ea8b417c8fc007c98a60bcefc8
illusionBoy311 <hbwhxgychy@163.com>
2021-06-20 00:06:30 +0800

    isValid

--- Leetcode/7_isValid.py
+++ Leetcode/7_isValid.py
@@ -17,19 +17,19 @@ class Solution:
         }
 
         stack = list()
-        # 遍历
+        # 遍历字符串
         for ch in s:
-            if ch in pairs:
+            if ch in pairs:  # 如果遇到右括号,需要匹配一个和右括号相同类型的左括号
                 if not stack or stack[-1] != pairs[ch]:
                     return False
                 stack.pop()
             else:
-                stack.append(ch)
+                stack.append(ch)  # 如果遇到左括号,直接入栈
         return not stack
 
 
 if __name__ == '__main__':
-    str1 = "()[]{}"
+    str1 = ""
     s = Solution()
     res = s.isValid(str1)
     print(res)
